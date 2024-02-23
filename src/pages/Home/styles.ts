00a6b2287798fd230f07de717d618bf8ae3d93ac
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-02-22 21:12:15 -0300

    feat: estilizando inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -78,3 +78,32 @@ export const StartCountdownButton = styled.button`
     cursor: not-allowed;
   }
 `;
+
+const BaseInput = styled.input`
+  background-color: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  background-color: transparent;
+  width: 4rem;
+`;
